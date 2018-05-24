8e49c7f8c3e3855b853c9a0c5999aa18b5429f04
Yaswanth reddy Desam <yaswanth@yaswanth.rsrit-students>
2018-05-24 11:52:43 -0400

    Initial commit

---
+++ sendmail.php
@@ -0,0 +1,28 @@
+
+<?php
+// Note: filter_var() requires PHP >= 5.2.0
+if ( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['comments']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+
+    // detect & prevent header injections
+    $mailTest = "/(content-type|bcc:|cc:|to:)/i";
+    foreach ( $_POST as $key => $val ) {
+        if ( preg_match( $mailTest, $val ) ) {
+            exit;
+        }
+    }
+
+    $headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
+        'Reply-To: ' . $_POST["email"] . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    $success = mail( "namana.fed@gmail.com", $_POST['subject'], $_POST['comments'], $headers );
+    //  Replace with your email
+
+    if ($success) {
+        ?>
+        <div style="color:#3c763d;padding:30px;text-align:center">
+            <strong>Success!</strong> Message has been sent successfully.
+        </div>
+    <?php
+    }
+}
